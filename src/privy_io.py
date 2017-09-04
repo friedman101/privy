8cdb3f950fb03cd2d0941398b91876c9b7212a13
david <dhfriedman@gmail.com>
2017-09-04 13:28:49 -0700

    make io into class

--- src/privy_io.py
+++ src/privy_io.py
@@ -1,31 +1,45 @@
 import socket
 
-def send(s, msg):
-    try:
-        s.send(msg.encode())
-    except:
-        return
+class PrivyIO:
+    def __init__(self, isServer, port, ip):
+        self.s = self.setup_socket()
 
-def receive(s):
-    try:
-        data = s.recv(1024)
-    except:
-        return
-    if not data:
-        return
+        if isServer:
+            self.s = self.config_server_socket(port)
+        else:
+            self.config_client_socket(ip, port)
 
-    return data.decode()
+        self.s.setblocking(0)
 
-def setup_socket():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    return s
+    def send(self, msg):
+        try:
+            self.s.send(msg.encode())
+        except:
+            return
 
-def config_client_socket(s, ip, TCP_PORT):
-    s.connect((ip, TCP_PORT))
+    def receive(self):
+        try:
+            data = self.s.recv(1024)
+        except:
+            return
+        if not data:
+            return
 
-def config_server_socket(s, TCP_PORT):
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('127.0.0.1', TCP_PORT))
-    s.listen(1)
-    s, ip = s.accept()
-    return s
+        return data.decode()
+
+    def close_socket(self):
+        self.s.close()
+
+    def setup_socket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        return s
+
+    def config_client_socket(self, ip, port):
+        self.s.connect((ip, port))
+
+    def config_server_socket(self, port):
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(('127.0.0.1', port))
+        self.s.listen(1)
+        self.s, ip = self.s.accept()
+        return self.s
